27a982f4d6b2ea9c97f21e4c1e459b4e04fe13bc
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2022-04-23 20:58:56 +0300

    progress vercel

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const chalk = require('chalk')
 const cors = require('cors')
 const routes = require('./routes/index')
@@ -8,18 +9,21 @@ const path = require('path')
 const initDatabase = require('./startUp/initDatabase')
 
 const app = express()
-const PORT = config.get('port') ?? 8080
+// const PORT = config.get('port') ?? 8080
+const PORT = process.env.PORT ?? 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
 app.use('/api', routes)
 
+dotenv.config()
+
 if (process.env.NODE_ENV === 'production') {
 	console.log('production')
-	app.use('/', express.static(path.join(__dirname, 'client/build')))
+	app.use('/', express.static(path.join(__dirname, '../client', 'build')))
 
-	const indexpath = path.join(__dirname, 'client/build/index.html')
+	const indexpath = path.join(__dirname, '../client', 'build', 'index.html')
 
 	app.get('*', (req, res) => {
 		res.sendFile(indexpath)
@@ -30,8 +34,14 @@ if (process.env.NODE_ENV === 'production') {
 
 async function start() {
 	try {
-		mongoose.connection.once('open', () => { initDatabase()})
-		await mongoose.connect(config.get('mongoUri'))
+		mongoose.connection.once('open', () => {
+			initDatabase()
+		})
+		// await mongoose.connect(config.get('mongoUri'))
+		await mongoose.connect(process.env.MONGOURI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
 		console.log(chalk.bgMagenta.black(`Mongo DataBase connected...`))
 		app.listen(PORT, () => {
 			console.log(chalk.bgYellow.black(`Server has been started on port: ${PORT}...`))})
